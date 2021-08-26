39571cd39b6bcfd9f5cfad7a6f05ae4a0cc8ccdb
sebastianbm <bmsebastian2@gmail.com>
2021-08-26 10:05:08 -0300

    Updates

---
+++ static/js/main.8d746ad9.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfirescmpleto=this.webpackJsonpfirescmpleto||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),s=c(3),i=c.n(s),o=c(0);var p=function(){return Object(o.jsx)("div",{})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(p,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.8d746ad9.chunk.js.map
